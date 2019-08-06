abbb458f8b046dd328a664bfc5da1dbd2e434488
Volozeleshyn <volozeleshyn@gmail.com>
2019-08-06 11:01:45 +0300

    Initial Commit

---
+++ Golden Hedgehog - Arcade/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Golden Hedgehog - Arcade
+//
+//  Created by Alexey Zelentsov on 06/08/2019.
+//  Copyright © 2019 Alexey Zelentsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
